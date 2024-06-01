380067ceec23ba2bde2f11badf3d1424400d3e41
yusuf <ysfnc27@gmail.com>
2024-06-01 12:15:38 +0300

    Proje dosyası ekle.

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using Ecommerce.Data;
+using Ecommerce.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+
+{
+	public class CategoryController : Controller
+	{
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+        public IActionResult Index() {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid) {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created";
+                return RedirectToAction("Index");
+            }
+            return View(); 
+
+        }
+
+        public IActionResult Edit(int categoryId)
+        {   
+            if (categoryId == null || categoryId == 0) {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(categoryId);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Edited";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(categoryId);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? categoryId)
+        {
+            Category obj = _db.Categories.Find(categoryId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+
+            TempData["success"] = "Category Deleted";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+	public class ApplicationDbContext : DbContext
+
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+			
+		
+		}
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                
+                new Category {CategoryId = 1, Name = "Tech", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "Dress", DisplayOrder = 2 },
+                new Category { CategoryId = 3, Name = "Book", DisplayOrder = 3 }
+
+                );
+        }
+
+    }
+}
